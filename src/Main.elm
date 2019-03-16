e870dcf67c9eabe47577954fcebfb1dd87412b8c
Rémi Lefèvre <rlefevre@gmail.com>
2019-03-16 02:52:45 +0100

    put client instructions in a dedicated module

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,7 @@ import Elm.Type as Type
 import Errors exposing (viewError)
 import File exposing (File)
 import File.Select as Select
+import Howto exposing (howto, howtoWithModules)
 import Html exposing (Html, a, div, h1, li, span, text, ul)
 import Html.Attributes exposing (class, href, id, style, title)
 import Html.Events exposing (on, onClick, preventDefaultOn)
@@ -368,70 +369,6 @@ compilationError error =
         , style "margin-top" "24px"
         ]
         [ viewError error ]
-
-
-howto : String
-howto =
-    """
-# Elm Documentation Previewer
-
-Open a package `README.md`, `docs.json` (from `elm make --docs=docs.json`) or both:
-
-> Click **Open Files** on the side or **drag & drop files** anywhere in the page.  
-> Invalid files will be ignored.
-
-## Sharing documentation
-
-To share some unpublished package documentation (for example for review), commit `README.md` and `docs.json` (generated with `elm make --docs docs.json`) to a github repository and use the following query parameters:
-
-**`repo`**
-> The github repository, for example `dmy/elm-doc-example`.
-
-**`version`** (optional, `master` by default)
-> The branch, tag or commit hash.
-
-Examples with https://github.com/dmy/elm-doc-example:
-* https://elm-doc-preview.netlify.com?repo=dmy/elm-doc-example
-* https://elm-doc-preview.netlify.com?repo=dmy/elm-doc-example&version=master
-* https://elm-doc-preview.netlify.com?repo=dmy/elm-doc-example&version=ba1054
-* https://elm-doc-preview.netlify.com/ModuleA?repo=dmy/elm-doc-example
-* https://elm-doc-preview.netlify.com/ModuleB?repo=dmy/elm-doc-example#TypeB
-
-Notes:
-* Paths to modules and symbols fragments are supported, so links from the documentation can be copied and shared.
-* Files not found or invalid will be ignored.
-
-## Privacy
-
-No data is sent to the server, so you can safely preview private packages documentation.
-
-Local documentation is stored in the browser local storage to improve navigation.
-Closing the preview clears it.
-
-## Local version with hot reloading
-When editing a package documentation, it is more convenient to see updates in real-time. For this you can use the local version that supports hot reloading, see https://www.npmjs.com/package/elm-doc-preview.
-
-## Credits
-
-* Documentation rendering from [package.elm-lang.org](https://github.com/elm/package.elm-lang.org) by Evan Czaplicki.
-* Markdown rendering from [Marked.js](https://github.com/markedjs/marked) by Christopher Jeffrey.
-* Code highlighting from [highlight.js](https://github.com/highlightjs/highlight.js) by Ivan Sagalaev.
-* Code highlighting theme from [Solarized](https://ethanschoonover.com/solarized) by Jeremy Hull.
-* CSS spinner from [SpinKit](https://github.com/tobiasahlin/SpinKit) by Tobias Ahlin.
-
-## Feedback
-
-Report bugs or feature requests at https://github.com/dmy/elm-doc-preview/issues.
-"""
-
-
-howtoWithModules : String
-howtoWithModules =
-    """
-# Elm Documentation Previewer
-
-Select a module or add a `README.md` file.
-"""
 
 
 doc : List Docs.Module -> Docs.Module -> String -> List (Html msg)
